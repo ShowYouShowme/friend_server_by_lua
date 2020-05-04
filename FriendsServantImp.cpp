e4819197eb8e187885718e5bef5d6bf4b241cf65
richie <861949775@qq.com>
2020-05-04 09:55:54 +0800

    定义接口，通过编译

--- FriendsServantImp.cpp
+++ FriendsServantImp.cpp
@@ -17,3 +17,25 @@ void FriendsServantImp::destroy()
     //...
 }
 
+
+tars::Int32 FriendsServantImp::AddFriend(const Friends::AddFriendReq& req, Friends::AddFriendResp& resp, tars::TarsCurrentPtr current)
+{
+    return 0;
+}
+tars::Int32 FriendsServantImp::DeleteFriend(const Friends::DeleteFriendReq& req, Friends::DeleteFriendResp& resp, tars::TarsCurrentPtr current)
+{
+    return 0;
+}
+tars::Int32 FriendsServantImp::QueryFriends(const Friends::QueryFriendListReq &req,Friends::QueryFriendListResp &resp,tars::TarsCurrentPtr current)
+{
+    return 0;
+}
+tars::Int32 FriendsServantImp::AgreeToAdd(const Friends::AgreeToAddReq & req,Friends::AgreeToAddResp &resp,tars::TarsCurrentPtr current)
+{
+    return 0;
+}
+tars::Int32 FriendsServantImp::GetApplicantList(const Friends::QueryApplicantListReq & req,Friends::QueryApplicantListResp &resp,tars::TarsCurrentPtr current)
+{
+    return 0;
+}
+
--- FriendsServantImp.h
+++ FriendsServantImp.h
@@ -29,7 +29,11 @@ public:
     /**
      *
      */
-    virtual int test(tars::TarsCurrentPtr current) { return 0;};
+    virtual tars::Int32 AddFriend(const Friends::AddFriendReq& req, Friends::AddFriendResp& resp, tars::TarsCurrentPtr current);
+    virtual tars::Int32 DeleteFriend(const Friends::DeleteFriendReq& req, Friends::DeleteFriendResp& resp, tars::TarsCurrentPtr current);
+    virtual tars::Int32 QueryFriends(const Friends::QueryFriendListReq &req,Friends::QueryFriendListResp &resp,tars::TarsCurrentPtr current);
+    virtual tars::Int32 AgreeToAdd(const Friends::AgreeToAddReq & req,Friends::AgreeToAddResp &resp,tars::TarsCurrentPtr current);
+    virtual tars::Int32 GetApplicantList(const Friends::QueryApplicantListReq & req,Friends::QueryApplicantListResp &resp,tars::TarsCurrentPtr current);
 };
 /////////////////////////////////////////////////////
 #endif
