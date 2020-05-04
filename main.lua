bc2fcd4d637aa85d93e58a6c59be4ba92da2c80d
richie <861949775@qq.com>
2020-05-04 18:02:44 +0800

    增加检查两人是否为好友的函数

--- main.lua
+++ main.lua
@@ -6,27 +6,36 @@ print("Hello from lua!")
 info = json.decode('{"name":"nash", "age": 28}')
 print(info["name"])
 
-
-function AddFriend(uid, friend_uid)
-    Log("log from lua!")
-
-    -- 这里应该返回两个值,一个是错误码,一个是返回值
+function checkBeenFriend( uid, friend_uid )
     local result = query("select * from tb_friend where uid = " .. uid .. " and friend_uid = " .. friend_uid)
     Log("result : " .. result)
 
     local records = json.decode(result)
     if #records ~= 1 then
         Log(uid .. " and " .. friend_uid .. " are not friends")
+        return false
     else
-        local relationship = records[1]["relationship"]
-        local _uid = records[1]["uid"]
-        local _friend_uid = records[1]["friend_uid"]
-        Log("===> relationship : " .. relationship .. " uid : " .. _uid .. " friend_uid : " .. _friend_uid)
-        if relationship == 1 then
-            Log("++++=1")
-        elseif relationship == "1" then
-            Log("]]]]]]]]]")
+        local relationship = tonumber(records[1]["relationship"])
+        if relationship == 3 then
+            return true
+        else
+            return false
         end
     end
+end
+
+
+function AddFriend(uid, friend_uid)
+    if uid == friend_uid then
+        Log("can not add yourself!")
+        return -100
+    end
+
+    -- 检查是否为好友
+    if checkBeenFriend(uid, friend_uid) == true then
+        return 0
+    end
+    -- 这里应该返回两个值,一个是错误码,一个是返回值
+
     return 1024
 end
